4a3394891eebd93b9246f1b8b41a765ac69d7bee
Oussama <oussama.aitbaziz@gmail.com>
2020-02-20 10:35:56 +0100

    added back-to-top button

--- js/script.js
+++ js/script.js
@@ -2,7 +2,11 @@ $(window).scroll(function() {
     var scroll = $(window).scrollTop();
     if (scroll >= 500) {
         $(".menu").addClass("fixed-top");
+        $(".back-to-top").addClass("animate");
+        $(".back-to-top").css("visibility", "visible");
     } else {
         $(".menu").removeClass("fixed-top");
+        $(".back-to-top").removeClass("animate");
+        $(".back-to-top").css("visibility", "hidden");
     }
 });
